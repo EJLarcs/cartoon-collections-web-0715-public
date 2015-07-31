0ce0ee3862ecc5c8f63eda61c8f7197c709563b3
EJLarcs <ejlarcombe@gmail.com>
2015-07-31 16:34:16 -0400

    cartoon collections

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf)
+# code an argument here
+  # Your code here
+  dwarf_names = []
+  dwarf.each_with_index { |name, index| 
+    dwarf_names << "#{index+1}. #{name}"
+  }
+  dwarf_names.each { |name| puts name}
+end
+
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |element| element.capitalize + "!" }
+# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planteer_calls(calls)
+  calls.any? { |n| n.length > 4}
 end
 
-def long_planteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  results = snacks & cheese_types
+  results[0]
 end
+
+#select vs. find. using select will give you an array of 
+#ALL even numbers, find will give you the FIRST match
